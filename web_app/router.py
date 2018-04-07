cdf0be5f1635e52e99c191a93237172897ac9a06
aks3011 <111501031@smail.iitpkd.ac.in>
2018-04-07 11:16:25 +0530

    Added movie vote system

--- web_app/router.py
+++ web_app/router.py
@@ -2,10 +2,14 @@ import web
 from controllers.hello import Hello
 from controllers.movie import Movie
 from controllers.award import Award
+from controllers.movie_user import Movie_user
+from controllers.comment_user import Comment_user
 
 urls = (
 	'/award/(.+)', 'Award',
 	'/movie/(.+)', 'Movie',
+	'/movie_user/(.+)', 'Movie_user',
+	'/comment_user/(.+)', 'Comment_user',
     '/(.*)', 'Hello'
 )
 
