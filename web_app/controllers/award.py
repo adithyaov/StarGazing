be9c7ca081db3520b5903e211f705e75ed52464f
aks3011 <111501031@smail.iitpkd.ac.in>
2018-04-07 09:22:32 +0530

    CRUD award done

--- web_app/controllers/award.py
+++ web_app/controllers/award.py
@@ -1,58 +1,67 @@
 import web
 from queries import *
 
-render = web.template.render('templates/')
+render_deep = web.template.render('templates/award/')
+render_shallow = web.template.render('templates/')
+db = web.database(dbn='mysql', user='root', pw='kidvscat', db='stargazing')
 
 class Award:
 	def GET(self, action_type):
 		data = web.input()
 		if action_type == 'R':
 			id = data.id
-			return render.test(read_query({
-				'selection': '*',
-				'main_tbl': 'Award',
-				'join_tbls': [],
-				'criteria': [
-					('Award', 'id', '==', id)
-				]
-			}))
+			return render_shallow.general_display(list(db.query(read_query({
+							'selection': '*',
+							'main_tbl': 'Award',
+							'join_tbls': [],
+							'criteria': [
+								('Award', 'id', '=', id)
+							]
+						}))))
+
+		if action_type == 'CRUD':
+			return render_deep.crud_award()
 
 	def POST(self, action_type):
 
 		data = web.input()
 
 		if action_type == 'C':
+			id = data.id
 			award_name = data.award_name
 			award_description = data.award_description
-			return insert_query({
+			db.query(insert_query({
 				'table': 'Award',
-				'k_list': ['award_name', 'award_description'],
-				'v_list': [award_name, award_description]
-			})
+				'k_list': ['id', 'award_name', 'award_description'],
+				'v_list': [id, award_name, award_description]
+			}))
+			return web.seeother('/award/R?id={}'.format(id))
 
 		if action_type == 'U':
 			id = data.id
 			award_name = data.award_name
 			award_description = data.award_description
-			return update_query({
+			db.query(update_query({
 				'table': 'Award',
 				'update_tuples': [
 					('award_name', award_name),
 					('award_description', award_description)
 				],
 				'criteria': [
-					('Award', 'id', '==', id)
+					('Award', 'id', '=', id)
 				]
-			})
+			}))
+			return web.seeother('/award/R?id={}'.format(id))
 
 		if action_type == 'D':			
 			id = data.id
-			return delete_query({
+			db.query(delete_query({
 				'table': 'Award',
 				'criteria': [
-					('Award', 'id', '==', id)
+					('Award', 'id', '=', id)
 				]
-			})
+			}))
+			return web.seeother('/award/CRUD')
 
 
 
--- web_app/router.py
+++ web_app/router.py
@@ -1,8 +1,10 @@
 import web
 from controllers.hello import Hello
 from controllers.movie import Movie
+from controllers.award import Award
 
 urls = (
+	'/award/(.+)', 'Award',
 	'/test/(.+)', 'Movie',
     '/(.*)', 'Hello'
 )
